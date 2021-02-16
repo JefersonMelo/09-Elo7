006bf90f5a7127e49abc75dd506066f32efa3923
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-16 18:48:27 -0300

    commit inicial

---
+++ Explorar_Marte/src/explorar_marte/Explorar_Marte.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package explorar_marte;
+import java.util.Locale;
+import java.util.Scanner;
+
+/**
+ * @author JefMelo
+ */
+public class Explorar_Marte {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner scan = new Scanner(System.in);
+        scan.useLocale(Locale.ENGLISH);
+        
+        
+    }
+
+}
