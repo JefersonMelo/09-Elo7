afb23fb497c6f3b1d60668fc218b89a0cc1955fb
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-17 18:05:20 -0300

    Commit Final. Classes, PontosCardeais e Planalto foram para Sonda e e não são utilizadas. Mantidas para Futura Manutenção do código.

--- Explorar_Marte/src/explorar_marte/Planalto.java
+++ Explorar_Marte/src/explorar_marte/Planalto.java
@@ -1,30 +1,23 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package explorar_marte;
 
 /**
  * @author JefMelo
  */
-
 public class Planalto {
 
     private int largura;
     private int altura;
-    //private char pontosCardeais;
-    //char[][] malha2D;
+    char[][] malha2D;
+
     public Planalto() {
     }
 
     public Planalto(int largura, int altura) {
         this.largura = largura;
         this.altura = altura;
-        
+
     }
-   
+
     public int getLargura() {
         return largura;
     }
@@ -41,21 +34,11 @@ public class Planalto {
         this.altura = altura;
     }
 
-//    public char getPontosCardeais() {
-//        return pontosCardeais;
-//    }
-//
-//    public void setPontosCardeais(char pontosCardeais) {
-//        this.pontosCardeais = pontosCardeais;
-//    }
+    public char[][] getMalha2D() {
+        return malha2D;
+    }
 
-//    public char[][] getMalha2D() {
-//        return malha2D;
-//    }
-//
-//    public void setMalha2D(char[][] malha2D) {
-//        this.malha2D = malha2D;
-//    }
-    
-    
+    public void setMalha2D(char[][] malha2D) {
+        this.malha2D = malha2D;
+    }
 }
