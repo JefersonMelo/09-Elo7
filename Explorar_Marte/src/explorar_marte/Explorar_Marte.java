08ffc85a3ed1ef14f297971500ceaa32c5c16fc9
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-16 23:28:52 -0300

    commit com erro o array de objetos. criação da Classe pontos cardeais, Planalto, Sonda. após resolução da lista de objetos concluir movimentar sonda = M

--- Explorar_Marte/src/explorar_marte/Explorar_Marte.java
+++ Explorar_Marte/src/explorar_marte/Explorar_Marte.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 /**
  * @author JefMelo
  */
+//https://www.discoduroderoer.es/arrays-de-objetos-en-java/
 public class Explorar_Marte {
 
     public static void main(String[] args) {
@@ -18,6 +19,31 @@ public class Explorar_Marte {
         Scanner scan = new Scanner(System.in);
         scan.useLocale(Locale.ENGLISH);
         
+        Planalto p = new Planalto();
+        Sonda sonda[] =  new Sonda[2];
+        int cont = 0;
+        
+        do {
+            
+            System.out.println("Entre com os valores X e Y do Planalto");
+            String[] valor = scan.nextLine().split(" ");
+            p.Malha(Integer.parseInt(valor[0]), Integer.parseInt(valor[1]));
+            
+            System.out.println("Entre com a posição inicial da sonda. Eixo X, Y e "
+                    + "\n um Ponto Cardeal: N, S, E, W");
+            valor = scan.nextLine().split(" ");
+            sonda[cont].posicao(Integer.parseInt(valor[0]), Integer.parseInt(valor[1]), valor[2]);
+            
+            System.out.println("Controles: R - Right, L - Left e M - Move");
+            String mover = scan.nextLine();
+            sonda[cont].MovimentarSonda(mover);
+            
+        } while (cont < 2);
+        
+        for (var item : sonda) {
+            
+            System.out.println(item);
+        }
         
     }
 
