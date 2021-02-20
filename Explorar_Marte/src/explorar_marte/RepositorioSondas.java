f01736df0788a2d8fa9cc59bb4a4099233251264
Jeferson Melo <jefsdemelo@gmail.com>
2021-02-20 18:04:30 -0300

    Commit Da Classe RepositorioSondas, criada para remover o trecho de POG que estava no loop do main

---
+++ Explorar_Marte/src/explorar_marte/RepositorioSondas.java
@@ -0,0 +1,23 @@
+package explorar_marte;
+
+import java.util.ArrayList;
+
+/**
+ * @author JefMelo
+ */
+public class RepositorioSondas {
+
+    private final ArrayList<Sonda> listaSonda = new ArrayList<>();
+
+    // adiciona na lista
+    public void Adicionar(Sonda s) {
+
+        listaSonda.add(s);
+
+    }
+
+    public ArrayList<Sonda> Lista() {
+        return listaSonda;
+    }
+
+}
